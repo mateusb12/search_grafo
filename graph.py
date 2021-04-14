1fdbfd4606de116a860608dfeb23d03c59f28887
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2021-04-14 09:47:53 -0300

    Corrigir algorítmo BFS

--- graph.py
+++ graph.py
@@ -1,23 +1,11 @@
-from node import Node
+class Node:
+    def __init__(self, label: str):
+        self.label = label
+        self.value = float("inf")
+        self.connections = []
 
-
-def setup_question(input_graph):
-    nodes = dict((chr(i + 114), []) for i in range(8))
-
-    nodes['r'] = ['s', 'v']
-    nodes['s'] = ['r', 'w']
-    nodes['t'] = ['u', 'w', 'x']
-    nodes['u'] = ['t', 'x', 'y']
-    nodes['v'] = ['r']
-    nodes['w'] = ['s', 't', 'x']
-    nodes['x'] = ['t', 'u', 'w', 'y']
-    nodes['y'] = ['u', 'x']
-
-    all_nodes = dict((node, Node(node)) for node in nodes)
-
-    for node in all_nodes:
-        all_nodes[node].connections = [all_nodes[c] for c in nodes[node]]
-        input_graph.add_node(all_nodes[node])
+    def __str__(self):
+        return f"[{self.label}]"
 
 
 class Graph:
@@ -34,58 +22,60 @@ class Graph:
         return 'Graph: \n{}'.format(
             '\n'.join([f'{existing_node.label} - {existing_node.connections}' for existing_node in self.nodes]))
 
-# Método já printa a fila em cada iteração e mostra o valor
-    def start(self, node_source, node_destination):
-        source = destination = None
-        # Loop for retrieving source and destination objects
+    # Método já printa a fila em cada iteração e mostra o valor
+    def start(self, node_source: str):
+        generated_tree = []
+        source = None
         for node in self.nodes:
             if node.label == node_source:
                 source = node
-            if node.label == node_destination:
-                destination = node
 
-        if type(source) == Node and type(destination) == Node:
+        # Verificar se ambos foram encontrados, senão retornar que não existe no grafo
+        if type(source) == Node:
 
             source.value = 0
             queue = [source]
             visited = set()
 
-            print(f"INDO DE {source} PROCURANDO POR {destination}")
             while len(queue):
-                print("-------------------------------------------------")
-                print([f'{str(i)}({i.value})' for i in queue], 'fila')
-                print([f'{str(i)}({i.value})' for i in visited], 'visitado')
+                print(' - '.join([f'{str(i)}({i.value})' for i in queue]), 'queue')
+                print(' - '.join([f'{str(i)}({i.value})' for i in visited]), 'visited\n')
 
                 actual_node = queue[0]
                 queue = queue[1:]
-
-                # Search ended
-                if actual_node == destination:
-                    print("-----------------------FIM-----------------------")
-                    print([f'{str(i)}({i.value})' for i in visited], 'visitado')
-                    return True
 
                 if actual_node in visited:
                     continue
 
                 visited.add(actual_node)
 
-                # Loop through current node "children"
                 for child in actual_node.connections:
-                    print(str(actual_node), '->', str(child))
+                    child.value = min(child.value, actual_node.value + 1)
 
-                    if child not in visited:
-                        child.value = actual_node.value + 1
+                    if child not in queue and child not in visited:
+                        queue.append(child)
 
-                    queue.append(child)
-            print("-----------------------FIM-----------------------")
-            print([f'{str(i)}({i.value})' for i in visited], 'visitado')
 
-        return False
+def setup_question(input_graph: Graph):
+    nodes = dict((chr(i + 114), []) for i in range(8))
+
+    nodes['r'] = ['s', 'v']
+    nodes['s'] = ['w', 'r']
+    nodes['t'] = ['u', 'w', 'x']
+    nodes['u'] = ['t', 'x', 'y']
+    nodes['v'] = ['r']
+    nodes['w'] = ['s', 't', 'x']
+    nodes['x'] = ['t', 'u', 'w', 'y']
+    nodes['y'] = ['u', 'x']
+
+    all_nodes = dict((node, Node(node)) for node in nodes)
+
+    for node in all_nodes:
+        all_nodes[node].connections = [all_nodes[c] for c in nodes[node]]
+        input_graph.add_node(all_nodes[node])
 
 
 g = Graph()
 setup_question(g)
 
-print(g.start('s', 'y'))
-apple = 5 + 3
+g.start('v')
